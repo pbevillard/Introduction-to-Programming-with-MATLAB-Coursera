6fe141b72eae6a93131d3e197379e796dad7762a
Paul Bevillard <pbevillard@hotmail.com>
2025-03-11 22:00:22 -0400

    Update caesar.m

--- caesar.m
+++ caesar.m
@@ -1,5 +1,20 @@
 function coded = caesar(array, shift)
 
+% Caesar's cypher is the simplest encryption algorithm. It adds a fixed 
+% value to the ASCII (unicode) value of each character of a text. In other 
+% words, it shifts the characters. Decrypting a text is simply shifting it
+% back by the same amount, that is, it substracts the same value from the 
+% characters. Write a function called caesar that accepts two arguments: 
+% the first is the character vector to be encrypted, while  the second is 
+% the shift amount. The function returns the output argument coded, the 
+% encrypted text. The function needs to work with all the visible ASCII 
+% characters from space to ~. The ASCII codes of these are 32 through 126.
+% If the shifted code goes outside of this range, it should wrap around. 
+% For example, if we shift ~ by 1, the result should be space. If we shift 
+% space by -1, the result should be ~. Here are a few things you may want 
+% to try with MATLAB before starting on this assignment:
+
+
     % The ASCII codes boundary is 31 through 126
         lowerLimit = 31;
         upperLimit = 126;
@@ -16,19 +31,3 @@ function coded = caesar(array, shift)
         end
         coded = char(ASCII_values);
 end
-
-
-% Caesar's cypher is the simplest encryption algorithm. It adds a fixed 
-% value to the ASCII (unicode) value of each character of a text. In other 
-% words, it shifts the characters. Decrypting a text is simply shifting it
-% back by the same amount, that is, it substracts the same value from the 
-% characters. Write a function called caesar that accepts two arguments: 
-% the first is the character vector to be encrypted, while  the second is 
-% the shift amount. The function returns the output argument coded, the 
-% encrypted text. The function needs to work with all the visible ASCII 
-% characters from space to ~. The ASCII codes of these are 32 through 126.
-% If the shifted code goes outside of this range, it should wrap around. 
-% For example, if we shift ~ by 1, the result should be space. If we shift 
-% space by -1, the result should be ~. Here are a few things you may want 
-% to try with MATLAB before starting on this assignment:
-
