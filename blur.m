3b2871551041a19d680fa92f97d528bb848f27e6
Paul Bevillard <pbevillard@hotmail.com>
2025-03-11 21:59:40 -0400

    Update blur.m

--- blur.m
+++ blur.m
@@ -34,25 +34,3 @@ end
 fprintf("\n\n")
 output = uint8(img_mean_values);
 end
-
-
-
-
-
-
-
-
-
-%     img_copy = double(img);
-%     [rows, columns] = size(img_copy);
-%     B = nan(size(img_copy) + 2 * w);
-%     B(w + 1 : end - w, w + 1 : end - w) = img_copy;
-%     output = 0 * img_copy;
-% 
-%     for i = w + 1 : rows + w
-%       for j = w + 1 : columns + w
-%         tmp = B(i - w : i + w, j - w : j + w);
-%         output(i - w, j - w) = mean(tmp(~isnan(tmp)));
-%       end
-%     end
-% end
